f1221fa99a58804de0323bef1c4e3c427aaf99d8
Eduard Rudniev <eduardrudnev97@gmail.com>
2024-11-19 20:51:34 +0100

    algorithm that takes an array and moves all of the zeros to the end

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import { isSolved } from './src/ticTacToe'
 import { reverseString } from './src/reverseString'
 import { deepEqual } from './src/deepEqual'
 import { debounce } from './src/debounce'
+import { moveZeros } from './src/movingZerosToTheEnd'
 
-return { rot13, isSolved, reverseString, deepEqual, debounce }
+return { moveZeros, rot13, isSolved, reverseString, deepEqual, debounce }
