57ace7be8a30444d76e900004cae4e05d250d96e
Eduard Rudniev <eduardrudnev97@gmail.com>
2024-11-19 20:51:29 +0100

    algorithm that takes an array and moves all of the zeros to the end

---
+++ src/movingZerosToTheEnd.js
@@ -0,0 +1,24 @@
+// // https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
+// // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+// // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+function moveZeros(list = []) {
+  if (!list.length) {
+    return list
+  }
+  let result = []
+  let zerosCount = 0
+  for (let i = 0; i <= list.length - 1; i++) {
+    if (list[i] === 0) {
+      zerosCount += 1
+      continue
+    }
+    result.push(list[i])
+  }
+  for (let i = 1; i <= zerosCount; i++) {
+    result.push(0)
+  }
+  return result
+}
+
+console.log(moveZeros(pattern))
