f205d958f7a4e244830d12693971097db36d859c
Eduard Rudnev <eduardrudnev97@gmail.com>
2024-11-20 18:33:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ import { reverseString } from './src/reverseString'
 import { deepEqual } from './src/deepEqual'
 import { debounce } from './src/debounce'
 import { moveZeros } from './src/movingZerosToTheEnd'
+import { toUnderscope, toUnderscope_v2 } src/toUnderscope.js
 
-return { moveZeros, rot13, isSolved, reverseString, deepEqual, debounce }
+return { moveZeros, rot13, isSolved, reverseString, deepEqual, debounce, toUnderscope, toUnderscope_v2 }
